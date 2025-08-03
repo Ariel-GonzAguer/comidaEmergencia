a4489b402f3f1712cb61933ce302fc0633c0fd8d
Ariel-GonzAguer <ariegonzaguer@gmail.com>
2025-08-02 18:30:55 -0600

    feat: add issue templates for bug reports and feature requests; enhance footer component by removing license information

--- src/componentes/Footer.jsx
+++ src/componentes/Footer.jsx
@@ -6,17 +6,7 @@ export default function Footer() {
   return (
     <footer className="border-t-2 border-light-primary text-text flex items-center justify-between p-1 w-full">
       <LogOutButton currentPath={window.location.pathname} />
-      <p>
-        Licencia AGPL-3.0.{" "}
-        <a
-          href="https://github.com/Ariel-GonzAguer/comidaEmergencia"
-          rel="noreferrer"
-          target="_blank"
-          className="text-light-primary font-bold hover:text-light-secundary hover:font-extrabold transition-colors duration-300"
-        >
-          Repositorio
-        </a>
-      </p>
+
       <button
         onClick={getFirebaseData}
         className="bg-blue-500 text-white p-2 rounded"
