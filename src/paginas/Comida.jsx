a299bc6ea2ad5fc64e619faf001fab7cc6c2fd3f
Ariel-GonzAguer <ariegonzaguer@gmail.com>
2025-08-07 18:05:50 -0600

    feat: add lint workflow for pull requests and remove unused store import in Comida component

--- src/paginas/Comida.jsx
+++ src/paginas/Comida.jsx
@@ -1,13 +1,7 @@
 // componentes
 import ComidaActual from "../componentes/ComidaActual";
 
-// store
-import useStore from "../stores/useStore";
-
 export default function Comida() {
-  // store
-  const { alimentos } = useStore();
-  // console.log("Comida:", alimentos);
 
   return <ComidaActual />;
 }
